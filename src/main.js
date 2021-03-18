2e775c7126cc431afff0b29c2b4c7a9045a4fe8a
tanimikyu <bloergesh@gmail.com>
2021-03-18 10:02:41 -0300

    JS inicial

--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+//declarar una variable - cuál es la longitud del arreglo
+//for para recorrer arreglo
+
+//let arreglo = [100, 101, 102]
+//console.log(arreglo.length)
+//console.log(arreglo[0])
+
 console.log(example, data);
+console.log(data.pokemon[9].name)
+console.log(data.pokemon[9].generation.name)
+console.log(data.pokemon[9].about)
