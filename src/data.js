1d8c786b47f5f04bf5b9c30ce94cb895354dd38c
tanimikyu <bloergesh@gmail.com>
2021-07-29 13:14:00 -0400

    final changes

--- src/data.js
+++ src/data.js
@@ -1,9 +1,8 @@
-
 //Función que filtra por Generación
 
 const filterByGeneration = (data, valor) => {
   const filterPokemonByGeneration = data.filter(
-    pokemon => pokemon.generation.name === valor,
+    (pokemon) => pokemon.generation.name === valor
   );
   return filterPokemonByGeneration;
 };
@@ -12,72 +11,66 @@ const filterByGeneration = (data, valor) => {
 
 const filterByType = (data, valor) => {
   const filterPokemonByType = data.filter(
-    pokemon => pokemon.type.indexOf(valor.toLowerCase()) > -1, 
+    (pokemon) => pokemon.type.indexOf(valor.toLowerCase()) > -1
   );
   return filterPokemonByType;
 };
 
 //Función para ordenar pokemon de A-Z y Z-A
-//data.sort = sort ordena el array según los parámetros que uno le ingrese 
+//data.sort = sort ordena el array según los parámetros que uno le ingrese
 
 const orderByName = (data, valor) => {
-  if (valor === 'A-Z') {
+  if (valor === "A-Z") {
     data.sort((a, b) => a.name.localeCompare(b.name));
   } else {
     data.sort((a, b) => b.name.localeCompare(a.name));
   }
 };
 
-
 //return -1 es porque a va antes que b
-//Funcion para ordenar pokemon por orden numerico 
+//Funcion para ordenar pokemon por orden numerico
 
 const orderByNum = (data, valor) => {
-  if (valor=== '0-9') {
-    
+  if (valor === "0-9") {
     //data.sort((a, b) => a.num.localeCompare(b.num));
 
     data.sort((a, b) => {
       if (a.num > b.num) {
-        return 1; 
-      }
-      else if (a.num < b.num){
+        return 1;
+      } else if (a.num < b.num) {
         return -1;
-      }
-      else if (a.num === b.num){
+      } else if (a.num === b.num) {
         return 0;
       }
-    })
-
+    });
   } else {
-    
     //data.sort((a, b) => b.num.localeCompare(a.num));
-  
+
     data.sort((a, b) => {
       if (a.num < b.num) {
-        return 1; 
-      }
-      else if (a.num > b.num){
+        return 1;
+      } else if (a.num > b.num) {
         return -1;
-      }
-      else if (a.num === b.num){
+      } else if (a.num === b.num) {
         return 0;
       }
-    })
+    });
   }
 };
 
-//Función de resistencia 
+//Función de resistencia
 
 const filterByResistant = (data, valor) => {
   const filterPokemonByResistant = data.filter(
-    pokemon => pokemon.resistant.indexOf(valor) > -1,
+    (pokemon) => pokemon.resistant.indexOf(valor) > -1
   );
   return filterPokemonByResistant;
 };
 
-
 export {
-  filterByGeneration, filterByType, orderByName, filterByResistant, orderByNum,
+  filterByGeneration,
+  filterByType,
+  orderByName,
+  filterByResistant,
+  orderByNum,
 };
-
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import data from './data/pokemon/pokemon.js';
+import data from "./data/pokemon/pokemon.js";
 import {
-  filterByGeneration, filterByType, orderByName, filterByResistant, orderByNum,
-} from './data.js';
+  filterByGeneration,
+  filterByType,
+  orderByName,
+  filterByResistant,
+  orderByNum,
+} from "./data.js";
 
 //Funcion desde Home a Pokedex General
 
-let btnPikachu = document.getElementById("pikachu")
-btnPikachu.addEventListener("click", nextPageOne)
+let btnPikachu = document.getElementById("pikachu");
+btnPikachu.addEventListener("click", nextPageOne);
 
 function nextPageOne() {
   document.getElementById("home").style.display = "none";
@@ -15,8 +19,8 @@ function nextPageOne() {
 
 //Funcion desde Pokedex a Home
 
-let btnBackDex = document.getElementById("btnBackDex")
-btnBackDex.addEventListener("click", homePageDex)
+let btnBackDex = document.getElementById("btnBackDex");
+btnBackDex.addEventListener("click", homePageDex);
 
 function homePageDex() {
   document.getElementById("segundaPagina").style.display = "none";
@@ -25,8 +29,8 @@ function homePageDex() {
 
 //Funcion desde Home a Botón Superior Izquierdo
 
-let btnLeft = document.getElementById("btnOne")
-btnLeft.addEventListener("click", nextPageLeft)
+let btnLeft = document.getElementById("btnOne");
+btnLeft.addEventListener("click", nextPageLeft);
 
 function nextPageLeft() {
   document.getElementById("home").style.display = "none";
@@ -35,8 +39,8 @@ function nextPageLeft() {
 
 //Funcion BACK Botón Superior Izquierdo
 
-let btnBack = document.getElementById("btnBack")
-btnBack.addEventListener("click", homePage)
+let btnBack = document.getElementById("btnBack");
+btnBack.addEventListener("click", homePage);
 
 function homePage() {
   document.getElementById("buttonLeft").style.display = "none";
@@ -45,18 +49,18 @@ function homePage() {
 
 //Funcion desde Home a Botón Superior Centro
 
-let btnCenter = document.getElementById("btnTwo")
-btnCenter.addEventListener("click", nextPageCenter)
+let btnCenter = document.getElementById("btnTwo");
+btnCenter.addEventListener("click", nextPageCenter);
 
 function nextPageCenter() {
   document.getElementById("home").style.display = "none";
   document.getElementById("buttonCenter").style.display = "block";
 }
 
-//Funcion BACK Botón Superior Centro 
+//Funcion BACK Botón Superior Centro
 
-let btnBackOne = document.getElementById("btnBackOne")
-btnBackOne.addEventListener("click", homePageOne)
+let btnBackOne = document.getElementById("btnBackOne");
+btnBackOne.addEventListener("click", homePageOne);
 
 function homePageOne() {
   document.getElementById("buttonCenter").style.display = "none";
@@ -65,8 +69,8 @@ function homePageOne() {
 
 //Funcion desde Home a Botón Superior Derecha
 
-let btnRight = document.getElementById("btnTree")
-btnRight.addEventListener("click", nextPageRight)
+let btnRight = document.getElementById("btnTree");
+btnRight.addEventListener("click", nextPageRight);
 
 function nextPageRight() {
   document.getElementById("home").style.display = "none";
@@ -75,24 +79,23 @@ function nextPageRight() {
 
 //Funcion BACK Botón Superior Derecho
 
-let btnBackTwo = document.getElementById("btnBackTwo")
-btnBackTwo.addEventListener("click", homePageTwo)
+let btnBackTwo = document.getElementById("btnBackTwo");
+btnBackTwo.addEventListener("click", homePageTwo);
 
 function homePageTwo() {
   document.getElementById("buttonRight").style.display = "none";
   document.getElementById("home").style.display = "block";
 }
 
-
 //Variable que guarda la data Pokémon
 
 const pokemonList = data.pokemon;
 
 //ForEach se ejecuta por cada elemento del array.
-// Función para mostrar todos los pokémon, se utiliza innerHTML para mostrar la información en pantalla. 
+// Función para mostrar todos los pokémon, se utiliza innerHTML para mostrar la información en pantalla.
 
 const displayPokemon = (pokemonData) => {
-  const pokeCard = document.getElementById('pokemones');
+  const pokeCard = document.getElementById("pokemones");
   pokemonData.forEach((elem) => {
     pokeCard.innerHTML += `
     
@@ -106,9 +109,13 @@ const displayPokemon = (pokemonData) => {
 
       <div id="back" class=" back">
          <div class="infCardAbout">${elem.about}</div> 
-         <div class="infCardAbout"<br>Generación<br>${elem.generation.name}</div> 
+         <div class="infCardAbout"<br>Generación<br>${
+           elem.generation.name
+         }</div> 
          <div class="infCardAbout">Tipo de Huevo<br>${elem.egg}</div>
-         <div class="infCardAbout infCard">Fortalezas<br>${elem.resistant.join(', ')}</div>
+         <div class="infCardAbout infCard">Fortalezas<br>${elem.resistant.join(
+           ", "
+         )}</div>
       </div>
     </div>
   </div>
@@ -118,76 +125,65 @@ const displayPokemon = (pokemonData) => {
 
 displayPokemon(pokemonList);
 
-//Filtrar según generación 
+//Filtrar según generación
 
 let searchGeneration;
-const containerGeneration = document.getElementById('generation');
-containerGeneration.addEventListener('change', () => {
-  const closePokemones = document.getElementById('pokemones');
-  closePokemones.innerHTML = '';
+const containerGeneration = document.getElementById("generation");
+containerGeneration.addEventListener("change", () => {
+  const closePokemones = document.getElementById("pokemones");
+  closePokemones.innerHTML = "";
   searchGeneration = containerGeneration.value;
 
   displayPokemon(filterByGeneration(pokemonList, searchGeneration));
 });
 
-
-
-
 //Filtrar según orden
 
 let searchOrder;
-const containerOrder = document.getElementById('order');
-containerOrder.addEventListener('change', () => {
-  const closePokemones = document.getElementById('pokemones');
-  closePokemones.innerHTML = '';
+const containerOrder = document.getElementById("order");
+containerOrder.addEventListener("change", () => {
+  const closePokemones = document.getElementById("pokemones");
+  closePokemones.innerHTML = "";
   searchOrder = containerOrder.value;
 
   orderByName(pokemonList, searchOrder);
   displayPokemon(pokemonList);
 });
 
-
 //Filtrar según número
 
 let searchOrderNum;
-const containerOrderNum = document.getElementById('orderNum');
-containerOrderNum.addEventListener('change', () => {
-  const closePokemones = document.getElementById('pokemones');
-  closePokemones.innerHTML = '';
+const containerOrderNum = document.getElementById("orderNum");
+containerOrderNum.addEventListener("change", () => {
+  const closePokemones = document.getElementById("pokemones");
+  closePokemones.innerHTML = "";
   searchOrderNum = containerOrderNum.value;
 
   orderByNum(pokemonList, searchOrderNum);
   displayPokemon(pokemonList);
 });
 
-
-
 //Filtrar según tipo
-//innerHTML vacio reemplaza todo el contenido por un contenido a definir 
+//innerHTML vacio reemplaza todo el contenido por un contenido a definir
 
 let searchType;
-const containerType = document.getElementById('tipo');
-containerType.addEventListener('change', () => {
-  const closePokemones = document.getElementById('pokemones');
-  closePokemones.innerHTML = '<h1>HOLA</h1><h1>HOLA</h1><h1>HOLA</h1><h1>HOLA</h1>';
+const containerType = document.getElementById("tipo");
+containerType.addEventListener("change", () => {
+  const closePokemones = document.getElementById("pokemones");
+  closePokemones.innerHTML = " ";
   searchType = containerType.value;
 
   displayPokemon(filterByType(pokemonList, searchType));
 });
 
-
-
-//Filtrar según Resistencia 
+//Filtrar según Resistencia
 
 let searchResistant;
-const containerResistant = document.getElementById('resistencia');
-containerResistant.addEventListener('change', () => {
-  const closePokemones = document.getElementById('pokemones');
-  closePokemones.innetHTML = '';
-
-
+const containerResistant = document.getElementById("resistencia");
+containerResistant.addEventListener("change", () => {
+  const closePokemones = document.getElementById("pokemones");
+  closePokemones.innertHTML = "";
   searchOrder = containerOrder.value;
 
   displayPokemon(filterByResistant(pokemonList, searchResistant));
 });
-
